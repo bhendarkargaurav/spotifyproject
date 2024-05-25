2ca15cac8ce2589ef757af693eb6946992381291
Gaurav <gaurav.bhe.24@gmail.com>
2024-05-25 16:51:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let x;
+
+// x = document.getElementById('new')
+// x = document.getElementsByClassName('playlist');
+// x = document.getElementsByTagName('h1');
+x = document.querySelector('.items');
+
+
+console.log(x);
+// console.log(x[5]); 5 indicate the position of Id.
